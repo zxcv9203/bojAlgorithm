387b15c4c6617611cbfe34210aaa4c5f32697063
zxcv9203 <zxcv9203@naver.com>
2022-01-02 20:54:12 +0900

    1008 golang solved

---
+++ class1/1001/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	r *bufio.Reader
+	w *bufio.Writer
+)
+
+func init() {
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+}
+func main() {
+	var a, b int
+
+	defer func(w *bufio.Writer) {
+		err := w.Flush()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(w)
+	_, err := fmt.Fscan(r, &a, &b)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = fmt.Fprintln(w, a-b)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
