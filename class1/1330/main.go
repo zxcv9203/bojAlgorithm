1821bfbaab3f0ed9419bd926da974c257208d15a
zxcv9203 <zxcv9203@naver.com>
2022-01-06 01:01:21 +0900

    1330 1546

---
+++ class1/1330/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	r *bufio.Reader
+	w *bufio.Writer
+)
+
+func init() {
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+}
+
+func main() {
+	var a, b int
+	defer func(w *bufio.Writer) {
+		err := w.Flush()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(w)
+	_, err := fmt.Fscan(r, &a, &b)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if a < b {
+		_, err = fmt.Fprintln(w, "<")
+	} else if a > b {
+		_, err = fmt.Fprintln(w, ">")
+	} else {
+		_, err = fmt.Fprintln(w, "==")
+	}
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
