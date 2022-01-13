d19a2cdd1f4e128e310bf0f80c27947d3e246369
zxcv9203 <zxcv9203@naver.com>
2022-01-13 19:06:21 +0900

    2739 solved

---
+++ class1/2739/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	r *bufio.Reader
+	w *bufio.Writer
+)
+
+func init() {
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+}
+
+func main() {
+	n := 0
+	defer func(w *bufio.Writer) {
+		err := w.Flush()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(w)
+	_, err := fmt.Fscan(r, &n)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for i := 1; i <= 9; i++ {
+		_, err = fmt.Fprintf(w, "%d * %d = %d\n", n, i, n*i)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
