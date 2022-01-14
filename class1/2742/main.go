04c42862e814daca264efb68cbf7411a59a87d89
zxcv9203 <zxcv9203@naver.com>
2022-01-14 14:53:13 +0900

    2741 & 2742 solved

---
+++ class1/2742/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	r *bufio.Reader
+	w *bufio.Writer
+)
+
+func init() {
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+}
+
+func main() {
+	n := 0
+	defer func(w *bufio.Writer) {
+		err := w.Flush()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(w)
+	_, err := fmt.Fscan(r, &n)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for i := n; i >= 1; i-- {
+		_, err = fmt.Fprintln(w, i)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
